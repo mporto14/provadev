05ef3ac39c19619935cde6594f037e2b1838856d
unknown <mporto@gmail.com>
2018-09-02 11:27:59 -0300

    .

--- src/main/java/br/com/agibank/provadev/services/Relatorio.java
+++ src/main/java/br/com/agibank/provadev/services/Relatorio.java
@@ -1,7 +1,6 @@
 package br.com.agibank.provadev.services;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
